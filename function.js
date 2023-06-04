f7bb11576aa663be684a98c54ca04c3ee2dbeee3
escalona-mj <116271291+escalona-mj@users.noreply.github.com>
2023-06-04 10:26:27 +0800

    testing function.js

--- function.js
+++ function.js
@@ -4,3 +4,7 @@ ScrollReveal({distance: '50px', duration: 2000, delay: 450});
 ScrollReveal().reveal('.hero-text',{delay:200, origin: 'top'});
 ScrollReveal().reveal('.hero-video',{delay:450, origin: 'bottom'});
 ScrollReveal().reveal('header',{delay:500, origin: 'top'});
+
+function testAlert() {
+    alert("No link yet :(");
+}
