50c6323faff30fe59c5af732b8ce62847e7ead8e
escalona-mj <116271291+escalona-mj@users.noreply.github.com>
2023-06-04 09:23:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# alarmingasylum.github.io
+Website for Mobile Systems and Technologies game project, Alarming Asylum.
+
+Visit the website here: https://escalona-mj.github.io/alarmingasylum.github.io/
---
+++ function.js
@@ -0,0 +1,6 @@
+//using the ScrollReveal from the external js script hosted in unpkg
+ScrollReveal({distance: '50px', duration: 2000, delay: 450});
+
+ScrollReveal().reveal('.hero-text',{delay:200, origin: 'top'});
+ScrollReveal().reveal('.hero-video',{delay:450, origin: 'bottom'});
+ScrollReveal().reveal('header',{delay:500, origin: 'top'});
